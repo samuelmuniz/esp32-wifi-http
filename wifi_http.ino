6ced972eabbe0350e812d4adbc87a1d897eeb78a
Samuel Muniz Silva <67869454+samuelmuniz@users.noreply.github.com>
2023-05-24 21:33:10 +0200

    Update add router

--- wifi_http.ino
+++ wifi_http.ino
@@ -1,133 +1,172 @@
-#include <WiFi.h> //add library
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESP.h>
 
-#define relay1 21 // pin for relay control 1
-#define relay2 22 // pin for relay control 2
 
-const char *ssid = "********";     // assign WiFi network name
-const char *password = "********"; // assign WiFi network password
+const int LED_BUILTIN = 2;
+const char* ssid = "ESPWifi";
+const char* password = "1234";
+AsyncWebServer server(80);
 
-unsigned long myTime = 0;
+//standart IP = 192.168.4.1
 
-WiFiServer server(80); // standard http port
+void testLocalCredentials(){
 
-void menu() // future function for for a changeable network name and password
-{
-}
-
-void relay_wifi()
-{
-
-  WiFiClient client = server.available(); // checks if there is a connected client with data to be transmitted
-
-  if (client) // there is a client ?
-  {           // YEP
-    Serial.println("NEW CLIENT");
-    String currentLine = ""; // store the mensagem from the client
-
-    while (client.connected()) // keep it meanwhile the client is connected
-    {
-
-      if (client.available())   // there is a mensage?
-      {                         // YEP
-        char c = client.read(); // stores in the CHAR variable C
-        Serial.write(c);
-
-        if (c == '\n')                   // new line ?
-        {                                // YEP
-          if (currentLine.length() == 0) // mensage finished ?
-          {                              // sim
-            // gera a página HTML
-            client.println("HTTP/1.1 200 OK"); // starts HTTP PROTOCOL
-            client.println("Content-type:text/html");
-            client.println();
-            client.print("<h2>type<a href=\"/H1\">HERE</a> to turn the relay 1 on </h2><br>"); // send messsage by the serial port
-            client.print("<h2>type<a href=\"/L1\">HERE</a> to turn the relay 1 off </h2><br>");
-            client.print("<h2>type<a href=\"/H2\">HERE</a> to turn the relay 2 on </h2><br>");
-            client.print("<h2>type<a href=\"/L2\">HERE</a> to turn the relay 2 off </h2><br>");
-            client.println();
-            break;
-
-          } // end if currentLine.length
-
-          else
-            currentLine = ""; // NOP, prevents the string of stay void.
-
-        } // end if c
-
-        else if (c != '\r')
-        {
-          currentLine += c; // add each element of the message
-        }
-
-        // check the mensage to turn on or off each relay
-        if (currentLine.endsWith("GET /H1"))
-        {
-          digitalWrite(relay1, HIGH);
-        }
-        if (currentLine.endsWith("GET /L1"))
-        {
-          digitalWrite(relay1, LOW);
-        }
-        if (currentLine.endsWith("GET /H2"))
-        {
-          digitalWrite(relay2, HIGH);
-        }
-        if (currentLine.endsWith("GET /L2"))
-        {
-          digitalWrite(relay2, LOW);
-        }
-
-      } // end if client.available()
-
-    } // end if while client.connected
-
-    client.stop(); // stops connection
-    Serial.println("Client Disconnected.");
-  }
-}
-
-void setup()
-{
-  Serial.begin(115200);    // inicializa Serial em 115200 baud rate
-  pinMode(relay1, OUTPUT); // configura saída para relé 1
-  pinMode(relay2, OUTPUT); // configura saída para relé 2
-
-  Serial.println();                 //
-  Serial.print("Conectando-se a "); //
-  Serial.println(ssid);             //
-  WiFi.begin(ssid, password);       // inicializa WiFi, passando o nome da rede e a senha
-
-  while (WiFi.status() != WL_CONNECTED) // aguarda conexão (WL_CONNECTED é uma constante que indica sucesso na conexão)
-  {
-    if (millis() - myTime >= 1000)
-    {
-      Serial.print(".");
-      myTime = millis();
+  server.on("/ssid", HTTP_POST, [](AsyncWebServerRequest *request){
+    String ssid_value;
+    String password_value;
+    //If the there are both "ssid" and "password" values
+    if (request->hasParam("ssid", true) && request->hasParam("password", true)) {
+      ssid_value = request->getParam("ssid", true)->value();
+      password_value = request->getParam("password", true)->value();
+      Serial.print("SSID: ");
+      Serial.println(ssid_value);
+      Serial.print("Password: ");
+      Serial.println(password_value);
     }
-  }
+    request->send(200, "text/html", "SSID: " + ssid_value + "<br>Password: " + password_value);
+  });
 
-  Serial.println("");
-  Serial.println("WiFi connected");   
-  Serial.println("IP ADRESS: "); 
-  Serial.println(WiFi.localIP());     
-
-  server.begin(); // inicializa o servidor web
 }
 
-void loop()
-{
+void setup() {
+  WiFi.mode(WIFI_STA);
+  //Erase flash memory
+  Serial.begin(115200);
+  // set the built-in LED pin as an output
+  pinMode(LED_BUILTIN, OUTPUT); 
+  //delete old credentials
+  WiFi.disconnect(true);
+  // Start Access Point mode with defined name and password
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  //print SSID name
+  Serial.println(WiFi.softAPSSID());
+  // Print the IP address of the Access Point
+  Serial.print("Access Point started. IP address: ");
+  Serial.println(WiFi.softAPIP());
+  server.begin();
 
-  while (WiFi.status() != WL_CONNECTED) // wait connection (WL_CONNECTED = TRUE)
-  {
-    WiFi.begin(ssid, password); //Starts WIFI object
-    
-    if (millis() - myTime >= 1000)
-    {
-      Serial.print(".");
-      myTime = millis();
+  
+
+
+  // Serve the HTML form on the root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+
+    String html_page = R"=====(
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Local Network configuration</title>
+      <style>
+        body {
+          background-color: #f2f2f2;
+          font-family: Arial, sans-serif;
+        }
+        h1 {
+          text-align: center;
+          margin-top: 50px;
+        }
+        form {
+          margin: 50px auto;
+          max-width: 500px;
+          padding: 20px;
+          background-color: #fff;
+          border-radius: 5px;
+          box-shadow: 0 0 10px rgba(0,0,0,0.2);
+        }
+        label {
+          display: block;
+          font-size: 18px;
+          margin-bottom: 10px;
+        }
+        input[type="text"],
+        input[type="password"] {
+          display: block;
+          width: 100%;
+          padding: 10px;
+          margin-bottom: 20px;
+          font-size: 18px;
+          border-radius: 5px;
+          border: 1px solid #ccc;
+          box-sizing: border-box;
+        }
+        input[type="submit"] {
+          background-color: #4CAF50;
+          color: #fff;
+          border: none;
+          border-radius: 5px;
+          padding: 10px 20px;
+          font-size: 18px;
+          cursor: pointer;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>ESP32 Wi-Fi Configuration</h1>
+      <form method="post" action="/ssid">
+        <label for="ssid">SSID:</label>
+        <input type="text" id="ssid" name="ssid">
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password">
+        <input type="submit" value="Submit">
+      </form>
+    </body>
+    </html>
+    )=====";
+
+    request->send(200, "text/html", html_page);
+  });
+
+  // Handle the form submission and store the SSID and password in variables
+  server.on("/ssid", HTTP_POST, [](AsyncWebServerRequest *request){
+    String ssid_value;
+    String password_value;
+    //If the there are both "ssid" and "password" values
+    if (request->hasParam("ssid", true) && request->hasParam("password", true)) {
+      ssid_value = request->getParam("ssid", true)->value();
+      password_value = request->getParam("password", true)->value();
+      if (ssid_value == ssid && password_value == password){
+        String html_page = R"=====(
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="UTF-8">
+          <title>ESP32 Lamp Control</title>
+        </head>
+        <body>
+          <h1>ESP32 Lamp Control</h1>
+
+          <h2>Lamp 1</h2>
+          <button onclick="toggleLamp1()">Toggle</button>
+
+          <h2>Lamp 2</h2>
+          <button onclick="toggleLamp2()">Toggle</button>
+
+        </body>
+        </html>
+        )=====";
+
+        request->send(200, "text/html", html_page);
+
+      }
+      
     }
-  }
 
-  relay_wifi(); // control each relay
+  });
 
-} // end loop
+}
+
+void loop() {
+  
+  // turn the LED on
+  digitalWrite(LED_BUILTIN, HIGH); 
+  // wait for 0.5 second
+  delay(500); 
+  // turn the LED off
+  digitalWrite(LED_BUILTIN, LOW); 
+  // wait for 0.5 second
+  delay(500);
+
+}
